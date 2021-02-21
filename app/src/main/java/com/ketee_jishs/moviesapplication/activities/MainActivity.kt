58c8bc2cbcf68db073712915a20251e380c4919f
Ekaterina <ketee.android@yahoo.com>
2021-02-21 09:56:54 +0300

    Homework 6 is done

--- app/src/main/java/com/ketee_jishs/moviesapplication/activities/MainActivity.kt
+++ app/src/main/java/com/ketee_jishs/moviesapplication/activities/MainActivity.kt
@@ -1,16 +1,24 @@
-package com.ketee_jishs.moviesapplication
+package com.ketee_jishs.moviesapplication.activities
 
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.ConnectivityManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import com.ketee_jishs.moviesapplication.R
+import com.ketee_jishs.moviesapplication.info.ConnectivityChange
 import kotlinx.android.synthetic.main.activity_main.*
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
+    private val connectivityChange = ConnectivityChange()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        onClickAbout()
         changeTheme()
     }
 
@@ -27,5 +35,23 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    private fun onClickAbout() {
+        imageButton.setOnClickListener {
+            startActivity(Intent(this, AboutActivity::class.java))
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val intentFilter = IntentFilter()
+        intentFilter.addAction(ConnectivityManager.CONNECTIVITY_ACTION)
+        registerReceiver(connectivityChange, intentFilter)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        unregisterReceiver(connectivityChange)
     }
 }
